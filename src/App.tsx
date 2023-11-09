d00c72c68aa72e32fa0b9a3684b61dadf0dbd894
Muhammad Umer <mhadisb1212@gmail.com>
2023-11-09 14:38:39 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex font-extrabold text-yellow-600 border">all good</div>
+    </>
+  );
+}
+
+export default App;
